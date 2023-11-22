35401b3615100aa6e37c25b7cdb4ca61ab4c76ee
Wavyness <drn.li@hotmail.com>
2023-11-22 19:05:35 +0100

    Initial Commit

---
+++ TODO3/TODO3App.swift
@@ -0,0 +1,17 @@
+//
+//  TODO3App.swift
+//  TODO3
+//
+//  Created by Dimitri Liauw on 22/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct TODO3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
