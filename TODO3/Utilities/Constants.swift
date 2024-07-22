070fd1f4db2adf2374e2368cbb6ea901e6981e90
Wavyness <drn.li@hotmail.com>
2024-07-22 22:34:38 +0200

    FEATURE #4: Added TODO3-API to this repo.
    Now we have a monolithic architecture.

--- TODO3/Utilities/Constants.swift
+++ TODO3/Utilities/Constants.swift
@@ -1,6 +1,7 @@
 //
 //  Constants.swift
 //  TODO3
+//  Default URL: http://127.0.0.1:8080/
 //
 //  Created by Dimitri Liauw on 23/11/2023.
 //
@@ -8,7 +9,7 @@
 import Foundation
 
 enum Constants {
-    static let baseURL = "http://127.0.0.1:8080/"       // default http://127.0.0.1:8080/
+    static let baseURL = "http://127.0.0.1:8080/"
 }
 
 enum Endpoints {
